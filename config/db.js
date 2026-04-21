3abd904925bafcf80fdcca1546aaf51bb8d5ab0f
Diego <maximiliano2008castro@gmail.com>
2026-04-20 20:06:59 -0600

    Agregando seguridad SSL para TiDB

--- config/db.js
+++ config/db.js
@@ -6,10 +6,14 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 // Verificación rápida de conexión
